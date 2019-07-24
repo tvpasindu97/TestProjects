ed972e3178c2f3daa2567e8b831d70f62085dc52
Pasindu Madushan <madushan@eight25media.com>
2019-07-24 13:54:28 +0530

    add new one

---
+++ Project 2/test.php
@@ -0,0 +1,11 @@
+<?php 
+require("vendor/autoload.php");
+
+use HelloWorld\SayHello;
+use dothemaths\maths;
+
+echo SayHello::world();
+echo maths::addvalue(5,6); echo " ";
+echo maths::addvalue(45,12); echo " ";
+echo maths::addvalue(20,20); echo " ";
+echo maths::addvalue(30,6);
